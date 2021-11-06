cfda6a4d58f2d12f4f0c83caba7d0436e1b78e9d
Micheal kuo <sxdcfv20002000@gmail.com>
2021-11-06 15:16:34 +0800

    “firstTry“

--- src/App.js
+++ src/App.js
@@ -1,92 +1,42 @@
-// 使用套件
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  useLocation,
-} from 'react-router-dom'
-import React, { useState } from 'react'
-import './Styles/global.scss'
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import AdoptList from './pages/AdoptList/AdoptList'
+import AdoptPage from './pages/AdoptPage/AdoptPage'
+import HotelList from './pages/HotelList/HotelList'
+import HotelPage from './pages/HotelPage/HotelPage'
+import ScrollToTop from 'react-router-scroll-top'
 
-// 頁面用元件
-import Home from './pages/Home/Home'
-import AdoptList from './pages/AdoptList'
-import ProductList from './pages/ProductList'
-import ProductDog from './pages/ProductDog'
-import ProductCat from './pages/ProductCat'
-import HotelList from './pages/HotelList'
-import BlogList from './pages/BlogList/BlogList'
-import Member from './pages/Member'
-import NotFoundPage from './pages/NotFoundPage'
-
-// 組合用元件
+// 組件
 import MyNavbar from './components/MyNavbar'
 import MyFooter from './components/MyFooter'
-import MainContent from './components/MainContent'
-import ScrollToTop from './components/ScrollToTop'
-//import BreadCrumb from './components/BreadCrumb'
 import MultiLevelBreadCrumb from './components/MultiLevelBreadCrumb'
-
-function App(props) {
-  const [auth, setAuth] = useState(false)
-
+function App() {
   return (
     <Router>
       <>
-        {/* LOGO+標題+導覽列+上方選單 */}
-        <MyNavbar auth={auth} />
-        {/* 主內容區 */}
-        <MainContent>
-          <MultiLevelBreadCrumb />
-          {/* 匹配路由表(路徑單一匹配) */}
-          {/* 切換顯示的元件畫面放在這下面 */}
-          {/* ScrollToTop是為了讓連到另一頁內容時，頁面回到最上方 */}
-          <ScrollToTop>
-            <Switch>
-              <Route path="/bloglist">
-                <BlogList />
-              </Route>
-              <Route path="/productlist/dog">
-                <ProductDog />
-              </Route>
-              <Route path="/productlist/cat">
-                <ProductCat />
-              </Route>
-              <Route path="/productlist">
-                <ProductList />
-              </Route>
-              <Route path="/hotellist">
-                <HotelList auth={auth} />
-              </Route>
-              <Route path="/adoptlist">
-                <AdoptList auth={auth} />
-              </Route>
-              <Route
-                path="/home"
-                // className={
-                //   location.pathname === '/home'
-                //     ? 'MultiLevelBreadCrumb-hidden'
-                //     : 'MultiLevelBreadCrumb-show'
-                // }
-              >
-                <Home auth={auth} />
-              </Route>
-              <Route path="/member">
-                <Member auth={auth} />
-              </Route>
-              <Route path="*">
-                <NotFoundPage />
-              </Route>
+        <MyNavbar />
+        <MultiLevelBreadCrumb />
+        <ScrollToTop>
+          <Switch>
+            <Route path="/adoptlist/adoptpage/:id?">
+              <AdoptPage />
+            </Route>
+            <Route path="/hotellist/hotelpage/:id?">
+              <HotelPage />
+            </Route>
+            <Route path="/adoptlist/">
+              <AdoptList />
+            </Route>
+            <Route path="/hotellist">
+              <HotelList />
+            </Route>
+            {/* <Route path="/upload">
+            <DataThings />
+          </Route> */}
+          </Switch>
+        </ScrollToTop>
 
-              {/* 這裡要定義網址參數的屬性名稱 */}
-              {/* <Route path="/product/baby/:id?">
-                <ProductBaby />
-              </Route> */}
-            </Switch>
-            {/* end 匹配路由表 */}
-          </ScrollToTop>
-        </MainContent>
-        {/* 頁尾+版權訊息 */}
+        {/* end 匹配路由表 */}
         <MyFooter />
       </>
     </Router>
